ae38e7cde5161a6d55be4e34cce3f67857fe6ce9
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-23 18:07:22 +0530

    adding +2 with every chars

--- problem2.py
+++ problem2.py
@@ -1,25 +1,26 @@
-sentence = input("Enter a line :")
+sentence = input("Enter a line :")  # asking the user to give a sentence
 
-splited_words= sentence.split(" ")
-final_string=""
-for i in range(len(splited_words)):
-    if i % 2 == 0:
-        word=splited_words[i]
-        for j in range(len(word)):
-            let_acssi=ord(word[j])
-            let_acssi+=2            
-            final_string=final_string+chr(let_acssi)
-        final_string+=" "
-    elif i % 2 !=0:
-        word=splited_words[i]
-        word=word[::-1]
-        for j in range(len(word)):
-            let_acssi=ord(word[j])
-            let_acssi+=2
-            final_string=final_string+chr(let_acssi)
-        final_string+=" "
+splited_words= sentence.split(" ")  # spliting it and stored in another variable
+final_string="" # declaring the another string
+for i in range(len(splited_words)): # this loop for traverse the list
+    if i % 2 == 0:  # if i % 2 is 0 then it is even
+        word=splited_words[i]   # storing the word in word variable
+        for j in range(len(word)):  # traversing into the word 
+            let_acssi=ord(word[j])  # converting characters into numbers
+            let_acssi+=2            # adding +2 with it 
+            final_string=final_string+chr(let_acssi)    # strong in final string with number convert into char
+        final_string+=" "   # adding space to it
         
-print(final_string)
+    elif i % 2 !=0:     # if i % 2 not equal to 0
+        word=splited_words[i]   # storing word in word varibale
+        word=word[::-1] # reversing the word
+        for j in range(len(word)):  # traversing into the word
+            let_acssi=ord(word[j])  # converting evry chars into numbers
+            let_acssi+=2    # adding + 2 with every chars
+            final_string=final_string+chr(let_acssi)     # strong in final string with number convert into char
+        final_string+=" "   # adding space to it
+        
+print(final_string) # displaing the final string
 
 
 # Output
