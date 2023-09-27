48a3453fe9f97ff8ad91d2f24ad539138bc0ecf6
krishnarajkumar0610 <krishanraj0610@gmail.com>
2023-09-27 16:05:44 +0530

    Exceptional handling

---
+++ Round 2/ExceptionHandling.py
@@ -0,0 +1,15 @@
+a = int(input("a : "))
+b = int(input("b : "))
+
+try:
+    c = a / b
+    print(c)
+    b = True
+except TypeError:
+    b = False
+    print(TypeError)
+finally:
+    if b:
+        print("Your calculation is done")
+    else:
+        print("It has error so can't calculate")
